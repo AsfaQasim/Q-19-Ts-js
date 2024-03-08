04e805b51ad684105164211896504d31706331f5
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-08 07:52:47 -0800

    Q 19 of Ts/js & node js of 45 assignment.
    Q 19 of Ts/js & node js of 45  assignment.

---
+++ main.js
@@ -0,0 +1,5 @@
+// 19.Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+// Define an array of places to visit
+var placesToVisit = ["Dubai", "Paris", "New York", "Makkah", "Burjh Khalifa"];
+// Print the number of dinner guests
+console.log("Number of dinner guests: ".concat(placesToVisit.length));
---
+++ main.ts
@@ -0,0 +1,9 @@
+
+// 19.Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+// Define an array of places to visit
+
+let placesToVisit: string[] = ["Dubai", "Paris", "New York", "Makkah", "Burjh Khalifa"];
+
+// Print the number of dinner guests
+console.log(`Number of dinner guests: ${placesToVisit.length}`);
